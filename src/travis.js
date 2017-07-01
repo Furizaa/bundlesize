21c9342667827613717e5d3d43b7969c41e5376a
Andreas Hoffmann <furizaa@gmail.com>
2017-07-01 19:57:44 +0200

    Test the stuff

--- src/travis.js
+++ src/travis.js
@@ -1,11 +1,9 @@
 const travis = {
-  repo: process.env.TRAVIS_REPO_SLUG,
+  repo: process.env.CIRCLE_REPOSITORY_URL,
   token: process.env.github_token,
-  event_type: process.env.TRAVIS_EVENT_TYPE,
-  sha: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT,
-  branch: process.env.TRAVIS_EVENT_TYPE === 'push'
-    ? process.env.TRAVIS_BRANCH
-    : process.env.TRAVIS_PULL_REQUEST_BRANCH
+  event_type: 'pull_request',
+  sha: process.env.CIRCLE_SHA1,
+  branch: process.env.CIRCLE_BRANCH
 }
 
 module.exports = travis
