bd6c68488af00ba2fc4f8697a20a344479dbd650
Andreas Hoffmann <furizaa@gmail.com>
2017-07-01 20:48:20 +0200

    Fix environment export

--- src/environment.js
+++ src/environment.js
@@ -1,7 +1,9 @@
+let environment;
+
 // Use CircleCi's env variables if detected.
 // See https://circleci.com/docs/1.0/environment-variables/ for reference.
 if (process.env.CIRCLECI) {
-  module.exports = {
+  environment = {
     repo: `${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}`,
     token: process.env.github_token,
     // Circle doesn't have the EVENT_TYPE information so we default to 'pull_request'.
@@ -9,17 +11,19 @@ if (process.env.CIRCLECI) {
     sha: process.env.CIRCLE_SHA1,
     branch: process.env.CIRCLE_BRANCH
   };
+} else {
+  // Default to travis
+  // See https://docs.travis-ci.com/user/environment-variables/ for reference.
+  // @TODO: Perhaps let the end-user know which environment is loaded.
+  environment = {
+    repo: process.env.TRAVIS_REPO_SLUG,
+    token: process.env.github_token,
+    event_type: process.env.TRAVIS_EVENT_TYPE,
+    sha: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT,
+    branch: process.env.TRAVIS_EVENT_TYPE === 'push'
+      ? process.env.TRAVIS_BRANCH
+      : process.env.TRAVIS_PULL_REQUEST_BRANCH
+  };
 }
 
-// Default to travis
-// See https://docs.travis-ci.com/user/environment-variables/ for reference.
-// @TODO: Perhaps let the end-user know which environment is loaded.
-module.exports = {
-  repo: process.env.TRAVIS_REPO_SLUG,
-  token: process.env.github_token,
-  event_type: process.env.TRAVIS_EVENT_TYPE,
-  sha: process.env.TRAVIS_PULL_REQUEST_SHA || process.env.TRAVIS_COMMIT,
-  branch: process.env.TRAVIS_EVENT_TYPE === 'push'
-    ? process.env.TRAVIS_BRANCH
-    : process.env.TRAVIS_PULL_REQUEST_BRANCH
-};
+module.exports = environment;
